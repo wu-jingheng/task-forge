a34cc270eb1bf542d8cdd3678e1265445abf17e0
jingheng.wu <wujingheng.j@gmail.com>
2025-06-14 17:39:03 +0800

    Removed additional .yml config files for now. Edited ci.yml. Added testcontainer dependency and adjusted main application test class.

--- src/test/java/com/moxie/task_forge/TaskForgeApplicationTests.java
+++ src/test/java/com/moxie/task_forge/TaskForgeApplicationTests.java
@@ -2,9 +2,28 @@ package com.moxie.task_forge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class TaskForgeApplicationTests {
+
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0")
+			.withReuse(true);
+
+	static {
+		mongoDBContainer.start();
+	}
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		String uri = mongoDBContainer.getReplicaSetUrl() + "task-forge-db";
+		System.out.println("MongoDB Testcontainer URI: " + uri);
+		registry.add("spring.data.mongodb.uri", () -> uri);
+	}
 
 	@Test
 	void contextLoads() {
