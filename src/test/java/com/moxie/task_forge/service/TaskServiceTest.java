4d3fa5a9ef5d89431d34a23400467e05d018fec2
jingheng.wu <wujingheng.j@gmail.com>
2025-06-14 14:47:44 +0800

    Added tests for TaskService

--- src/test/java/com/moxie/task_forge/service/TaskServiceTest.java
+++ src/test/java/com/moxie/task_forge/service/TaskServiceTest.java
@@ -1,7 +1,75 @@
 package com.moxie.task_forge.service;
 
+import com.moxie.task_forge.exception.TaskNotFoundException;
+import com.moxie.task_forge.model.Task;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskServiceTest {
 
+    @Test
+    void addAndGetTask_shouldWork() {
+        TaskService service = new TaskService();
+        Task task = new Task("1", "Test task", "Test description");
+
+        service.addTask(task);
+
+        Task retrievedTask = service.getTask("1");
+        assertEquals("Test task", retrievedTask.getTitle());
+    }
+
+    @Test
+    void updateAndGetTask_shouldWork() {
+        TaskService service = new TaskService();
+        Task task = new Task("1", "Test task", "Test description");
+        service.addTask(task);
+        task.setTitle("Updated task");
+
+        service.updateTask(task);
+
+        Task retrievedTask = service.getTask("1");
+        assertEquals("Updated task", retrievedTask.getTitle());
+    }
+
+    @Test
+    void updateAndGetTask_shouldThrow() {
+        TaskService service = new TaskService();
+        Task task = new Task("1", "Test task", "Test description");
+
+        assertThrows(TaskNotFoundException.class, () -> service.updateTask(task));
+    }
+
+    @Test
+    void deleteTask_shouldWork() {
+        TaskService service = new TaskService();
+        Task task = new Task("1", "Test task", "Test description");
+        service.addTask(task);
+
+        assertDoesNotThrow(() -> service.removeTask("1"));
+    }
+
+    @Test
+    void deleteTask_shouldThrow() {
+        TaskService service = new TaskService();
+
+        assertThrows(TaskNotFoundException.class, () -> service.removeTask("1"));
+    }
+
+    @Test
+    void listTasks_shouldWork() {
+        TaskService service = new TaskService();
+        Task task1 = new Task("1", "Test task", "Test description");
+        Task task2 = new Task("2", "Test task", "Test description");
+        Task task3 = new Task("3", "Test task", "Test description");
+        service.addTask(task1);
+        service.addTask(task2);
+        service.addTask(task3);
+
+        List<Task> retrievedTasks = service.listTasks();
+
+        assertEquals(3, retrievedTasks.size());
+    }
 }
